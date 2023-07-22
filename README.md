734c35d658761901a654be2f05e74e8eacafe10e
Arian000 <arian.banaee00@gmail.com>
2023-07-22 09:22:23 +0330

    readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# hermes-project-recommender
+Hermes Project Recommender
+Hermes Project Recommender is a Telegram bot that leverages OpenAI's powerful GPT-3 model to recommend projects to users based on their resume.
+
+The user submits a command to the bot in the format '/filename' where 'filename' is the name of the CSV file containing project descriptions. The bot reads the project descriptions and the user's resume text, and then uses GPT-3 to recommend projects that best align with the user's resume.
+
+Getting Started
+Prerequisites
+Python 3.7+
+pandas
+aiogram
+openai
+asyncio
+Install the prerequisites with pip:
+
+Copy code
+pip install pandas aiogram openai asyncio
+Setting Up
+Clone the repository to your local machine:
+bash
+Copy code
+git clone https://github.com/Arian000/hermes-project-recommender.git
+Navigate to the project directory:
+bash
+Copy code
+cd hermes-project-recommender
+Set up your Telegram bot and get your bot token.
+
+Get your OpenAI API key.
+
+Replace the placeholders <TELEGRAM-BOT-TOKEN> and <OPENAI-API-KEY> in the main.py file with your respective Telegram bot token and OpenAI API key.
+
+Running the Bot
+To start the bot, run the following command:
+
+css
+Copy code
+python main.py
+Usage
+Message the bot with a command in the format /filename where 'filename' is the name of the CSV file (without the .csv extension) containing project descriptions.
+
+The bot will read the project descriptions and the user's resume text (which should be in a text file named with the user's ID under the 'resumes' directory), and respond with the projects that best align with the user's resume.
+
+License
+This project is licensed under the terms of the MIT license.
+
+Contact
+For any questions or concerns, please open an issue on this GitHub repository.
--- bot.py
+++ bot.py
@@ -1,51 +1,68 @@
+# Import necessary libraries
 import pandas as pd
 import asyncio
 from aiogram import Bot, Dispatcher, types
 import openai
 
-telegram_token = '6018456454:AAG0dvTHEWHudF2loaY2QKutRMlCnEbHKCk'
-openai.api_key = ''
-# Initialize bot and dispatcher
+# Define Telegram bot token and OpenAI API key
+telegram_token = '<TELEGRAM-BOT-TOKEN>'
+openai.api_key = '<OPENAI-API-KEY>'
+
+# Initialize a Bot and a Dispatcher for handling incoming messages
 bot = Bot(token=telegram_token)
 dp = Dispatcher(bot)
 
+# Function to recommend projects based on user's resume
 def recommend(projects, resume_text):
-    system_text = f"You are a project recommender. User will give you a resume and you should sort the projects based on compatibality with the resume. These are description of projects: {str(projects)}"
+    # Prepare system message for GPT-3 with project descriptions
+    system_text = f"You are a project recommender. User will give you a resume and you should sort the projects based on compatibility with the resume. These are description of projects: {str(projects)}"
     messages = [
-            {"role": "system", "content": system_text},
-            {"role": "user", "content": resume_text},
-        ]
+        {"role": "system", "content": system_text},
+        {"role": "user", "content": resume_text},
+    ]
     
+    # Send request to GPT-3 API
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
+    # Return the response received from GPT-3
     return str(response["choices"][0]["message"]["content"])
 
+# Asynchronous message handler
 @dp.message_handler()
 async def handle_message(message: types.Message):
+    # Ignore messages that do not start with '/'
     if message.text[0] != '/':
         return
+    # Prepare filename of CSV file containing project descriptions
     filename = f"csvs/{message.text[1:]}.csv"
     projects = pd.read_csv(filename)['Description']
     try:
+        # Read user's resume text from file
         with open(f'resumes/{message.from_user.id}.txt', 'r') as file:
             resume_text = file.read().replace('\n', '')
+        # Get recommended projects
         projects_sorted = recommend(projects, resume_text)
         print(projects_sorted)
-        # for project in projects:
+        # Respond with the recommended projects
         await message.answer(projects_sorted)
+    # Catch exceptions and respond with the error message
     except Exception as e:
         await message.answer(f"Error: {str(e)}")
 
+# Function to send message when bot starts
 async def on_startup(dp):
     await bot.send_message(119298397, 'Your bot started')
 
+# Function to send message when bot stops
 async def on_shutdown(dp):
     await bot.send_message(119298397, 'Your bot stopped')
 
+# Main function
 if __name__ == '__main__':
     from aiogram import executor
 
+    # Start the bot and set up the startup and shutdown functions
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
